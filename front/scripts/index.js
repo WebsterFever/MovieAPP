5d072059531f2c8b1bb0ab984b8d3571d075b6d3
Webster Fever <webstersevenlanguages@gmail.com>
2026-02-16 20:56:00 -0500

    add challenge testing and using axios

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -2,6 +2,46 @@
 
 const renderCards = require("./renderCards");
 
+const axios = require('axios');
+
+//using async await
+
+const fetchMovies = async () => {
+    try {
+        const response = await axios.get("https://students-api.up.railway.app/movies");  
+        console.log(response.data);
+        renderCards(response.data);
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+    }
+};
+
+fetchMovies();
+
+/*
+const fetchData = () => {
+    return axios.get('https://students-api.up.railway.app/movies')
+        .then(response => response.data)  
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            throw error; 
+        });
+}
+
+fetchData()
+    .then(data => {
+        console.log('Data fetched successfully:', data);  
+        renderCards(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+  
+
+
+
+/usando ajax
 $.get("https://students-api.up.railway.app/movies", (data) => {
-  renderCards(data);
-});
+ renderCards(data);
+ });
+*/
