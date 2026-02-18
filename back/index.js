06ed31fd16ae37b7c2c38201e8be3bf43cb5b216
Webster Fever <webstersevenlanguages@gmail.com>
2026-02-17 23:49:41 -0500

    configurar controllers , routes , service , server

---
+++ back/index.js
@@ -0,0 +1,5 @@
+const app = require("./src/server");
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
---
+++ back/src/controllers/moviesController.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+ getAllMovies: (req, res) => {
+   res.status(200).json({ message: " próximamente estarán disponibles los datos de películas." });
+    }   
+};
